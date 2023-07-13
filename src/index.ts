6ffd4c6900a41ea0c932edb21866441c381db1e6
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-13 12:41:20 +0700

    create simple route for initial API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 
 const app: Application = express();
 const port: Number = 4000;
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ data: 'Hello World' });
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
