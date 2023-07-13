8c0bd7ef9f6fa8dc18dd0048f60b0fb7709fcbf1
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-13 17:03:24 +0700

    setup eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app: Application = express();
 const port: Number = 4000;
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: '200' });
+  res.status(200).send({ status: '200' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
