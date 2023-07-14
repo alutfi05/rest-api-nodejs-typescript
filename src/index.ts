ed76ca582afed87bece86ed65133dfb567efe671
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-14 15:50:22 +0700

    setup body-parser and cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import express from 'express'
-import type { Application } from 'express'
+import type { Application, Request, Response, NextFunction } from 'express'
 import { routes } from './routes'
 import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app: Application = express()
 const port: number = 4000
+
+// parse body request
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// cors access handler
+app.use(cors())
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  next()
+})
 
 routes(app)
 
