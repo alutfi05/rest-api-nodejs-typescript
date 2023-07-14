d85ce354d19b3f216b7f5ac0b2c82516f47bec28
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-14 17:00:12 +0700

    implements createProductValidation in product route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import type { NextFunction, Request, Response } from 'express'
 import { logger } from '../utils/logger'
+import { createProductValidation } from '../validation/product.validation'
 
 export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('Success get product data')
-  res.status(200).send({
+  return res.status(200).send({
     status: true,
     statusCode: 200,
     data: [{ name: 'Sepatu Sport', price: 500000 }]
@@ -14,10 +15,23 @@ ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
 })
 
 ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = createProductValidation(req.body)
+
+  if (error) {
+    logger.error(`ERR: product - create =  ${error.details[0].message}`)
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error.details[0].message,
+      data: {}
+    })
+  }
+
   logger.info('Success add new product')
-  res.status(200).send({
+  return res.status(200).send({
     status: true,
     statusCode: 200,
-    data: req.body
+    message: 'Add product success',
+    data: value
   })
 })
