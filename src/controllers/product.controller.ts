08dc62a7470fbbb61c6d6b635254677f685b62ec
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-17 12:24:01 +0700

    create service for adding product and implement into controller and fixing detail product

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,16 +1,11 @@
 import type { Request, Response } from 'express'
 import { createProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
-import { getProductFromDB } from '../services/product.service'
+import { addProductToDB, getProductById, getProductFromDB } from '../services/product.service'
+import { v4 as uuidv4 } from 'uuid'
 
-interface ProductType {
-  product_id: string
-  name: string
-  price: number
-  size: string
-}
-
-export const createProduct = (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response) => {
+  req.body.product_id = uuidv4()
   const { error, value } = createProductValidation(req.body)
 
   if (error) {
@@ -23,46 +18,56 @@ export const createProduct = (req: Request, res: Response) => {
     })
   }
 
-  logger.info('Success create new product')
-  return res.status(201).send({
-    status: true,
-    statusCode: 201,
-    message: 'Create product success',
-    data: value
-  })
+  try {
+    await addProductToDB(value)
+
+    logger.info('Success add new product')
+    return res.status(201).send({
+      status: true,
+      statusCode: 201,
+      message: 'Add product success'
+    })
+  } catch (error) {
+    logger.error(`ERR: product - create =  ${error}`)
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error
+    })
+  }
 }
 
 export const getProduct = async (req: Request, res: Response) => {
-  const products: any = await getProductFromDB()
-
   const {
-    params: { name }
+    params: { id }
   } = req
 
-  if (name) {
-    const filterProduct = products.filter((product: ProductType) => product.name === name)
+  if (id) {
+    const product: any = await getProductById(id)
 
-    if (filterProduct.length === 0) {
-      logger.info('Data not found')
+    if (product) {
+      logger.info('Success get product data')
+      return res.status(200).send({
+        status: true,
+        statusCode: 200,
+        data: product
+      })
+    } else {
       return res.status(404).send({
         status: false,
         statusCode: 404,
+        message: 'Data Not Found',
         data: {}
       })
     }
+  } else {
+    const products: any = await getProductFromDB()
 
-    logger.info('Success get product data')
+    logger.info('Success get all products data')
     return res.status(200).send({
       status: true,
       statusCode: 200,
-      data: filterProduct[0]
+      data: products
     })
   }
-
-  logger.info('Success get all products data')
-  return res.status(200).send({
-    status: true,
-    statusCode: 200,
-    data: products
-  })
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,5 +1,15 @@
 import { logger } from '../utils/logger'
 import productModel from '../models/product.model'
+import type ProductType from '../types/product.type'
+
+export const addProductToDB = async (payload: ProductType) => {
+  try {
+    return await productModel.create(payload)
+  } catch (error) {
+    logger.info('Cannot add data yo DB')
+    logger.error(error)
+  }
+}
 
 export const getProductFromDB = async () => {
   try {
@@ -7,6 +17,16 @@ export const getProductFromDB = async () => {
     return data
   } catch (error) {
     logger.info('Cannot get data from DB')
+    logger.error(error)
+  }
+}
+
+export const getProductById = async (id: string) => {
+  try {
+    const data = await productModel.findOne({ product_id: id })
+    return data
+  } catch (error) {
+    logger.info('Cannot get data by id from DB')
     logger.error(error)
   }
 }
