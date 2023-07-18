7706bf0a5079382a0ecf72fdcb82f0d036a67068
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-18 17:17:39 +0700

    check auto deploy

--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -6,5 +6,5 @@ export const HealthRouter: Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('Health check success')
-  res.status(200).json({ status: '200' })
+  res.status(200).json({ status: '200', message: 'Server is running' })
 })
