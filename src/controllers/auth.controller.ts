24ed061274a6f96afa6eac2419b24b3d1d8070cc
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-18 15:23:41 +0700

    create refresh token feature

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,10 @@
 import type { Request, Response } from 'express'
-import { createSessionValidation, createUserValidation } from '../validations/auth.validation'
+import { createSessionValidation, createUserValidation, refreshSessionValidation } from '../validations/auth.validation'
 import { v4 as uuidv4 } from 'uuid'
 import { logger } from '../utils/logger'
 import { checkPassword, hashing } from '../utils/hashing'
 import { createUser, findUserByEmail } from '../services/auth.service'
-import { signJWT } from '../utils/jwt'
+import { signJWT, verifyJWT } from '../utils/jwt'
 
 export const registerUser = async (req: Request, res: Response) => {
   req.body.user_id = uuidv4()
@@ -59,15 +59,55 @@ export const createSession = async (req: Request, res: Response) => {
     }
 
     const accessToken = signJWT({ ...user }, { expiresIn: '1d' })
+    const refreshToken = signJWT({ ...user }, { expiresIn: '1y' })
 
     return res.status(200).json({
       status: true,
       statusCode: 200,
       message: 'Login Success',
-      data: { accessToken }
+      data: { accessToken, refreshToken }
     })
   } catch (error: any) {
     logger.error(`ERR: auth - create session =  ${error.message}`)
+    return res.status(422).json({
+      status: false,
+      statusCode: 422,
+      message: error.message
+    })
+  }
+}
+
+export const refreshSession = async (req: Request, res: Response) => {
+  const { error, value } = refreshSessionValidation(req.body)
+
+  if (error) {
+    logger.error(`ERR: auth - refresh session =  ${error.details[0].message}`)
+    return res.status(422).json({
+      status: false,
+      statusCode: 422,
+      message: error.details[0].message
+    })
+  }
+
+  try {
+    const { decoded } = verifyJWT(value.refreshToken)
+
+    const user = await findUserByEmail(decoded._doc.email)
+
+    if (!user) {
+      return false
+    }
+
+    const accessToken = signJWT({ ...user }, { expiresIn: '1d' })
+
+    return res.status(200).json({
+      status: true,
+      statusCode: 200,
+      message: 'Refresh Session Success',
+      data: { accessToken }
+    })
+  } catch (error: any) {
+    logger.error(`ERR: auth - refresh session =  ${error.message}`)
     return res.status(422).json({
       status: false,
       statusCode: 422,
