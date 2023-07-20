e6e7f107c1d934e5e8b9d961fe073ebcc8efa503
Ahmad Lutfi Rizki Patria <aluthfi0522@gmail.com>
2023-07-20 12:52:04 +0700

    create unit testing for createProduct, if login as admin or reguler

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -5,10 +5,15 @@ import type { Application } from 'express'
 import { v4 as uuidv4 } from 'uuid'
 import createServer from '../utils/server'
 import { addProductToDB } from '../services/product.service'
+import { createUser } from '../services/auth.service'
+import { hashing } from '../utils/hashing'
 
 const app: Application = createServer()
 
 const productId1 = uuidv4()
+const productId2 = uuidv4()
+const userIdAdmin = uuidv4()
+const userIdReguler = uuidv4()
 
 const productPayload = {
   product_id: productId1,
@@ -17,11 +22,46 @@ const productPayload = {
   size: 'L'
 }
 
+const productPayloadCreate = {
+  product_id: productId2,
+  name: 'Hoodie Putih',
+  price: 499000,
+  size: 'L'
+}
+
+const userAdminCreated = {
+  user_id: userIdAdmin,
+  email: 'aluthfi123@gmail.com',
+  name: 'Ahmad Lutfi Rizki Patria',
+  password: `${hashing('test123')}`,
+  role: 'admin'
+}
+
+const userCreated = {
+  user_id: userIdReguler,
+  email: 'aluthfi0522@gmail.com',
+  name: 'Ahmad Lutfi Rizki Patria',
+  password: `${hashing('test123')}`,
+  role: 'regular'
+}
+
+const userAdmin = {
+  email: 'aluthfi123@gmail.com',
+  password: 'test123'
+}
+
+const userReguler = {
+  email: 'aluthfi0522@gmail.com',
+  password: 'test123'
+}
+
 describe('product', () => {
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create()
     await mongoose.connect(mongoServer.getUri())
     await addProductToDB(productPayload)
+    await createUser(userAdminCreated)
+    await createUser(userCreated)
   })
 
   afterAll(async () => {
@@ -50,6 +90,43 @@ describe('product', () => {
         const { statusCode, body } = await supertest(app).get(`/product/${productId1}`)
         expect(statusCode).toBe(200)
         expect(body.data.name).toBe('Hoodie Hitam')
+      })
+    })
+  })
+
+  describe('create product', () => {
+    describe('if user is not login', () => {
+      it('should return 403, request forbidden', async () => {
+        const { statusCode } = await supertest(app).post('/product').send(productPayloadCreate)
+        expect(statusCode).toBe(403)
+      })
+    })
+    describe('if user is login as admin', () => {
+      it('should return 201, success create product', async () => {
+        const { body } = await supertest(app).post('/auth/login').send(userAdmin)
+        const { statusCode } = await supertest(app)
+          .post('/product')
+          .set('Authorization', `Bearer ${body.data.accessToken}`)
+          .send(productPayloadCreate)
+        expect(statusCode).toBe(201)
+      })
+      // it('should return 422, product name is exist in DB', async () => {
+      //   const { body } = await supertest(app).post('/auth/login').send(userAdmin)
+      //   const { statusCode } = await supertest(app)
+      //     .post('/product')
+      //     .set('Authorization', `Bearer ${body.data.accessToken}`)
+      //     .send(productPayload)
+      //   expect(statusCode).toBe(422)
+      // })
+    })
+    describe('if user is login as reguler', () => {
+      it('should return 403, request forbidden', async () => {
+        const { body } = await supertest(app).post('/auth/login').send(userReguler)
+        const { statusCode } = await supertest(app)
+          .post('/product')
+          .set('Authorization', `Bearer ${body.data.accessToken}`)
+          .send(productPayloadCreate)
+        expect(statusCode).toBe(403)
       })
     })
   })
